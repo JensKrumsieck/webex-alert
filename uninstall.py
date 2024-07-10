858844c7f82fd16b0facbc2975ee1b498f54e414
Jens Krumsieck <jens.krumsieck@thuenen.de>
2024-07-10 09:14:42 +0000

    fix uninstall script

--- uninstall.py
+++ uninstall.py
@@ -3,7 +3,10 @@ from __util import comment
 
 cron = CronTab(user=True)
 
-cron.remove_all()
+for job in cron:
+    if comment in job.comment:
+        cron.remove(job)
+
 cron.write()
 print("Removed Cronjob!")
 
